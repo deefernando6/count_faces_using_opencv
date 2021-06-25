07a5446617667182af70ad202b0dbfde6c8bead8
deeghayu <deefernando6@gmail.com>
2021-06-26 00:01:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ face_cascade = cv2.CascadeClassifier("<location_to_open_cv>\opencv\data\haarcasc
 img=  cv2.imread("istockphoto-1032897084-640x640.jpg")
 
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+faces=face_cascade.detectMultiScale(gray,1.1,3)
