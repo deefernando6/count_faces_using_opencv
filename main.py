a5cc9fc0d82761da12d154b8efe18ecec6455666
deeghayu <deefernando6@gmail.com>
2021-06-26 00:02:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ img=  cv2.imread("istockphoto-1032897084-640x640.jpg")
 
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 faces=face_cascade.detectMultiScale(gray,1.1,3)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
