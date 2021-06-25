f0985c55e98164f5ad1c0c0efcb681061f384fb9
deeghayu <deefernando6@gmail.com>
2021-06-26 00:01:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier("<location_to_open_cv>\opencv\data\haarcascades\haarcascade_frontalface_default.xml")
 img=  cv2.imread("istockphoto-1032897084-640x640.jpg")
+
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
