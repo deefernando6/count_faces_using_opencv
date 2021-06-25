663a9e8dc2026843fbf7a87936a49be056d172fe
deeghayu <deefernando6@gmail.com>
2021-06-25 22:50:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier("<location_to_open_cv>\opencv\data\haarcascades\haarcascade_frontalface_default.xml")
+img=  cv2.imread("istockphoto-1032897084-640x640.jpg")
